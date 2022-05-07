0e03510041c333dca34701f12159812c4619a341
Han_Solo <103543064+EduardoInsper@users.noreply.github.com>
2022-05-07 20:08:32 -0300

    Update harvesine.py

---
+++ Funções/harvesine.py
@@ -0,0 +1,11 @@
+def haversine(raio, lat1, lon1, lat2, lon2):
+    lat1 = lat1 * math.pi / 180
+    lon1 = lon1 * math.pi / 180
+    lat2 = lat2 * math.pi / 180
+    lon2 = lon2 * math.pi / 180
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = raio * c
+    return d
